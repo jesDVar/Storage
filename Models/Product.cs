fa41c9d986675bf0f5c19e849326e8d6a1699752
Jesse Varis <jessevaris@hotmail.com>
2022-01-24 15:18:29 +0100

    Properties

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Storage.Models
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public int Price;
+        public DateTime OrderDate;
+        public string Category;
+        public string Shelf;
+        public int Count;
+        public string Description;
+    }
+}

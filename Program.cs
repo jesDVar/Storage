47b96bee520b9e5338ad71c1b6a009072b43d318
Jesse Varis <jessevaris@hotmail.com>
2022-01-25 22:35:15 +0100

    Changed Titel on index in views

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Storage.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<StorageContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StorageContext")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
